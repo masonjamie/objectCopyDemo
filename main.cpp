7a29cfa5354ebb9da9b4dd47c60556b9861e9878
jsmason <jamie.mason@socc.edu>
2021-03-17 16:49:08 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "Course.h"
+
+int main()
+{
+  Course course1("C++ Programming", 10);
+  Course course2(course1);
+
+  course1.addStudent("Peter Pan"); // Add a student to course1
+  course2.addStudent("Lisa Ma"); // Add a student to course2
+
+  std::cout << "students in course1: " <<
+    course1.getStudents()[0] << std::endl;
+  std::cout << "students in course2: " <<
+    course2.getStudents()[0] << std::endl;
+
+  return 0;
+}
+
